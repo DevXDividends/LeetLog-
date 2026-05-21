f0f1991c3ad6dab6769a638fc79dcadf7bca82c2
Aditya Dengale <adityadengale16@gmail.com>
2026-05-21 23:41:44 +0530

    update app.py with descriptions

--- app.py
+++ app.py
@@ -7,9 +7,27 @@ st.set_page_config(page_title="LeetLog",page_icon="💻",layout="wide")
 def load_data():
     with open("dsa_data.json",'r',encoding="utf-8") as file:
         return json.load(file)
+    
+def load_des():
+    with open("dsa_descriptions.json",'r')as file:
+        return json.load(file)
+    
+description = load_des()
 data = load_data()
 
-
+def get_problem_metadata(des,problem):
+    if problem in des:
+        return{
+            "title": des[problem]["title"],
+            "source":des[problem]["source"],
+            "description":des[problem]["description"],
+            "examples":des[problem]["examples"]
+        }
+    else:
+        return {
+            "title": "No data Found !"
+        }
+    
 st.sidebar.title("📂 LeetLog Archive")
 st.sidebar.markdown("---")
 
@@ -20,12 +38,41 @@ selected_category = st.sidebar.selectbox("Select Topic",folder)
 if selected_category:
     files = list(data[selected_category].keys())
 
-    selected_file = st.sidebar.radio("Select Problem",files)
+    selected_file = st.sidebar.radio(
+        "Select Problem",
+        files,
+        format_func=lambda x:x.replace(".cpp"," ")
+        )
 
 if selected_category and selected_file:
-    st.title(selected_file.replace(".cpp"," "))
+
+    des_data = get_problem_metadata(
+        des=description,
+        problem=selected_file
+        )
+    
+    st.title(des_data["title"].replace(".cpp",""))
+
     st.caption(f"Category: {selected_category}")
-    st.markdown("---")
+    st.write(f"Source: {des_data["source"]}")
+    st.subheader("Description")
+
+    st.write(des_data["description"])
+    st.subheader("Examples")
+
+    for idx, ex in enumerate(des_data["examples"], start=1):
+     with st.container(border=True):
+
+        st.markdown(f"### Example {idx}")
+
+        st.markdown("#### Input")
+        st.code(ex["input"], language="cpp")
+
+        st.markdown("#### Output")
+        st.code(ex["output"], language="cpp")
+
+        st.markdown("#### Explanation")
+        st.write(ex["explanation"])
 
     code_content = data[selected_category][selected_file]
     st.code(code_content,language="cpp")
