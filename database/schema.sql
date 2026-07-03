d0201fec78356ea3e2f318ad32cffde36c4bda03
Aditya Dengale <adityadengale16@gmail.com>
2026-07-03 20:11:51 +0530

    add new columns to problemss table and update type on another

--- database/schema.sql
+++ database/schema.sql
@@ -1,37 +1,54 @@
-create table
-    Problems (
-        id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-        title varchar(255) NOT NULL,
-        topic varchar(100) NOT NULL,
-        difficulty text NOT NULL,
-        source varchar(50) NOT NULL,
-         link TEXT,
-        description TEXT NOT NULL,
-        constraints TEXT NOT NULL,
-        examples JSONB NOT NULL,
-        editorial_code TEXT NOT NULL,
-        time_limit_ms INTEGER NOT NULL,
-        memory_limit_mb INTEGER NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
+CREATE TABLE Problems (
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 
-create table
-    StarterCode (
-        id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-        problem_id INTEGER NOT NULL,
-        CONSTRAINT fk_starter_problem FOREIGN KEY (problem_id) REFERENCES Problems (id) ON DELETE CASCADE,
-        language varchar(20) NOT NULL,
-        template_code TEXT NOT NULL,
-        wrapper_main TEXT NOT NULL
-    );
+    problem_key TEXT UNIQUE NOT NULL,
 
-create table
-    TestCases (
-        id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-        problem_id INTEGER NOT NULL,
-        CONSTRAINT fk_testcase_problem FOREIGN KEY (problem_id) REFERENCES Problems (id) ON DELETE CASCADE,
-        input TEXT NOT NULL,
-        expected_output TEXT NOT NULL,
-        is_hidden BOOLEAN NOT NULL DEFAULT FALSE
-    );
+    title VARCHAR(255) NOT NULL,
+    topic VARCHAR(100) NOT NULL,
+    difficulty TEXT NOT NULL,
+    source VARCHAR(50) NOT NULL,
+    link TEXT,
+
+    description TEXT NOT NULL,
+
+    constraints JSONB NOT NULL,
+    examples JSONB NOT NULL,
+
+    editorial_code TEXT NOT NULL,
+
+    time_limit_ms INTEGER NOT NULL,
+    memory_limit_mb INTEGER NOT NULL,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE StarterCode (
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
+    problem_id INTEGER NOT NULL,
+
+    CONSTRAINT fk_starter_problem
+        FOREIGN KEY (problem_id)
+        REFERENCES Problems(id)
+        ON DELETE CASCADE,
+
+    language VARCHAR(20) NOT NULL,
+    template_code TEXT NOT NULL,
+    wrapper_main TEXT NOT NULL
+);
+
+CREATE TABLE TestCases (
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
+    problem_id INTEGER NOT NULL,
+
+    CONSTRAINT fk_testcase_problem
+        FOREIGN KEY (problem_id)
+        REFERENCES Problems(id)
+        ON DELETE CASCADE,
+
+    input TEXT NOT NULL,
+    expected_output TEXT NOT NULL,
+    is_hidden BOOLEAN NOT NULL DEFAULT FALSE
+);
